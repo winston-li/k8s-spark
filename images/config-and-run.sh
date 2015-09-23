92e056eb52b2e6eb23bfcb5fe78f997c30952e1a
Winston Li <winston_li@hotmail.com>
2015-09-23 12:38:15 +0800

    revise port definition & use one k8s service only

--- images/config-and-run.sh
+++ images/config-and-run.sh
@@ -3,7 +3,7 @@
 if [ ! -z "$MASTER_ROLE" ]; then
   echo "Starting Master..."
   # spark-master k8s service redefined this variable, which is incompatible with Spark. Redefine it here. 
-  export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
+  export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT_CLUSTER_CMD:-7077}
   export SPARK_MASTER_IP="spark-master"
   export SPARK_PUBLIC_DNS="spark-master.${POD_NAMESPACE}.k8s"
   echo "SPARK_MASTER_HOST=${SPARK_MASTER_IP}" >> /opt/spark/conf/spark-env.sh
@@ -17,11 +17,13 @@ else
     sleep 30 # To postpone pod restart
     exit 1
   fi
+  export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT_CLUSTER_CMD:-7077}
   export SPARK_LOCAL_HOSTNAME=$(hostname)
-  export SPARK_PUBLIC_DNS="spark-$(echo $(hostname -i) | sed 's/\./-/g').${POD_NAMESPACE}.k8s"
+  #export SPARK_PUBLIC_DNS="spark-$(echo $(hostname -i) | sed 's/\./-/g').${POD_NAMESPACE}.k8s"
+  export SPARK_PUBLIC_DNS="spark.$(hostname).${POD_NAMESPACE}.k8s"
   echo "SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS}" >> /opt/spark/conf/spark-env.sh
   echo "${SPARK_MASTER_SERVICE_HOST} spark-master" >> /etc/hosts
-  /opt/spark/sbin/start-slave.sh spark://spark-master:${SPARK_MASTER_SERVICE_PORT}
+  /opt/spark/sbin/start-slave.sh spark://spark-master:${SPARK_MASTER_PORT}
 fi
 
 tail -F /spark_data/log/*
--- teardown.sh
+++ teardown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 kubectl delete service spark-master "$@"
-kubectl delete service spark-master-pub "$@"
 kubectl delete rc spark-master-rc "$@"
 kubectl delete rc spark-worker-rc "$@"
 
